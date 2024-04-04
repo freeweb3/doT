da48f0c304585dd6f3e91e54c537c6b5b675ba40
freeweb3 <166019866+freeweb3@users.noreply.github.com>
2024-04-05 08:56:20 +1030

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+{
+  "name": "Datajs",
+  "website": "https://twitter.com/Datajs",
+  "description": "Learn Javascript",
+  "logo": "https://raw.githubusercontent.com/freeweb3/doT/master/logo.png",
+  "twitter": "https://twitter.com/Datajs"
+}
